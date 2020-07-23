0bec15e272ae931c648e1c85d8bd0a2fd9365751
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-07-22 19:59:14 -0700

    tests are passing

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,9 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://en.wikipedia.org/wiki/Uladzimir_Nyaklyayew'
-URL = 'https://en.wikipedia.org/wiki/Washington_(state)'
-
 
 def get_soup(URL):
     response = requests.get(URL)
@@ -16,7 +13,7 @@ def get_soup(URL):
 def get_citations_list(soup):
     content = soup.find('div', id='content')
     citations = content.find_all('a', title='Wikipedia:Citation needed')
-    citations_text = [c.parent.parent.parent.text.replace(
+    citations_text = [c.parent.parent.parent.text.strip().replace(
         '[citation needed]', '') for c in citations]
 
     return citations_text
@@ -37,11 +34,3 @@ def get_citations_needed_report(URL):
         output += f'Citation needed for {c} \n'
 
     return output
-
-
-if __name__ == "__main__":
-    length = get_citations_needed_count(URL)
-    string = get_citations_needed_report(URL)
-
-    print(f'Number of citations needed {length}')
-    print(string)
